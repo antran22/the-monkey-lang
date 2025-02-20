97536bc6dcab4451c3834916198b491e2f5c189a
An, Tran Cong Viet <work@antran.tech>
2025-02-21 02:26:45 +0700

    test: Add unit tests for lexer functionality

---
+++ lexer/lexer_test.go
@@ -0,0 +1,34 @@
+package lexer
+
+import (
+	"monkey/token"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNextToken(t *testing.T) {
+	assert := assert.New(t)
+	input := "=+(){},;"
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := NewLexer(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+	}
+}
